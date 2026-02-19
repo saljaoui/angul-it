ade894cb337750326edcdcf43122d8accd606f5b
saljaoui <soufian2022ff@gmail.com>
2026-02-19 04:34:07 +0000

    `feat: Added new features and styles to the application, including a captcha challenge, result page, and updated navigation and layout.`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,33 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'home' },
-    {
-        path: 'home',
-        loadComponent: () =>
-            import('./features/home/home.component').then(m => m.HomeComponent),
-    }
+  // Default route
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+  // Pages
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'captcha',
+    loadComponent: () =>
+      import('./features/captcha/captcha.component').then(m => m.CaptchaComponent),
+  },
+
+  // Result must be protected (no direct access)
+  {
+    path: 'result',
+    // canActivate: [captchaCompleteGuard],
+    loadComponent: () =>
+      import('./features/result/result.component').then(m => m.ResultComponent),
+  },
+
+  // 404 (always last)
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/not-found/not-found.component').then(m => m.NotFoundComponent),
+  },
 ];

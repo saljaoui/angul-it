e7cacd4fee9e34cc998167a2e694016d7b83e606
saljaoui <soufian2022ff@gmail.com>
2026-02-20 01:54:17 +0000

    `Update Angular router configuration and add routerLink directives to components`

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
 import { Footer } from '../../shared/footer/footer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
